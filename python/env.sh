af82ccc4f3e888ec53345e0d67ffc68191038910
CYC <yenchen0416@gmail.com>
2021-04-15 15:46:41 +0800

    add: project python path

--- python/env.sh
+++ python/env.sh
@@ -1,3 +1,6 @@
 virtualenv --python=python3.6 --no-site-packages env3.6
+touch ./env3.6/lib/python3.6/site-packages/a.pth
+echo $PWD >> ./env3.6/lib/python3.6/site-packages/a.pth
+echo $PWD/**project_path** >> ./env3.6/lib/python3.6/site-packages/a.pth
 source env3.6/bin/activate
 pip3 install -r requirements.txt
